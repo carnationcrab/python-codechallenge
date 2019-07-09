9680acfb25f5885162bf221e9915e19ccbc71ade
Samantha Martorana <smartorana@Samanthas-MacBook-Pro.local>
2019-07-08 21:03:08 -0500

    Create DataCapture class
    Also initializes BuildStats class to be used by the build_stats
    method.

--- program.py
+++ program.py
@@ -1,5 +1,15 @@
 # DataCapture class that has add and build_stats methods
 
+class DataCapture:
+    lyst = []
+
+    def add(self, number):
+        self.lyst.append(number)
+
+    def build_stats(self):
+        return BuildStats(self.lyst)
+
+
 # BuildStats class that has less, greater, and between methods
 
 ### indigo's code ##
@@ -12,11 +22,11 @@ capture.add(3)
 capture.add(4)
 capture.add(6)
 
-stats = capture.build_stats()
+# stats = capture.build_stats()
 
-stats.less(4) # should return 2 (only two values 3,3 are less than 4)
+# stats.less(4) # should return 2 (only two values 3,3 are less than 4)
 
  
-stats.between(3, 6) # should return 4 (3,3,4 and 6 are between 3 and 6)
+# stats.between(3, 6) # should return 4 (3,3,4 and 6 are between 3 and 6)
 
-stats.greater(4) # should return 2 (6 and 9 are the only two values greater than 4)
+# stats.greater(4)) # should return 2 (6 and 9 are the only two values greater than 4)
